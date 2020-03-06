5ccb765b795e5f2ce4b6b17b28ec0a4f23b91fcc
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-03-06 11:56:53 +0530

    Email sending done

--- certificate.py
+++ certificate.py
@@ -1,20 +1,41 @@
 import xlrd
 import smtplib
-import ssl
 
 
-def getusers():
-    path = "ncc.xlsx"
-    inputWorkbook = xlrd.open_workbook(path)
-    inputWorksheet = inputWorkbook.sheet_by_index(0)
+path = "ncc.xlsx"
+inputWorkbook = xlrd.open_workbook(path)
+inputWorksheet = inputWorkbook.sheet_by_index(0)
 
-    userlist = []
+userlist = []
 
-    for i in range(inputWorksheet.nrows):
-        dictionary = {
-            "username1": inputWorksheet.cell_value(i, 1),
-            "username2": inputWorksheet.cell_value(i, 2),
-            "emailid": inputWorksheet.cell_value(i, 3),
+for i in range(inputWorksheet.nrows):
+	dictionary = {
+		"username1": inputWorksheet.cell_value(i, 1),
+		"username2": inputWorksheet.cell_value(i, 2),
+		"emailid": inputWorksheet.cell_value(i, 3),
 
-        }
-        userlist.append(dictionary)
+	}
+	userlist.append(dictionary)
+
+print(userlist)
+
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+     smtp.ehlo()
+     smtp.starttls()
+     smtp.ehlo()
+     
+     sender = "credenzusser@gmail.com"
+     receiver = "kajalparekh@gmail.com"
+     password = input("Enter your password:- ")
+     
+     smtp.login(sender,password)
+     
+     subject = "Regarding NCC"
+     body = "Thank you participating in NCC 2020. \nHere is your certificate"
+     
+     msg = f'Subject: {subject}\n\n{body}'
+     
+     smtp.sendmail(sender, receiver, msg)
+     
+
+
