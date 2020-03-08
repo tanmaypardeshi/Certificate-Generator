0eebc65eb2a51aefd6b3a3e2d6f1cc4762a0ad8d
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-03-08 14:52:16 +0530

    Added image

--- certificate.py
+++ certificate.py
@@ -1,41 +1,32 @@
 import xlrd
 import smtplib
+import getpass
 
 
 path = "ncc.xlsx"
 inputWorkbook = xlrd.open_workbook(path)
 inputWorksheet = inputWorkbook.sheet_by_index(0)
 
-userlist = []
-
-for i in range(inputWorksheet.nrows):
-	dictionary = {
-		"username1": inputWorksheet.cell_value(i, 1),
-		"username2": inputWorksheet.cell_value(i, 2),
-		"emailid": inputWorksheet.cell_value(i, 3),
-
-	}
-	userlist.append(dictionary)
-
-print(userlist)
 
 with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-     smtp.ehlo()
-     smtp.starttls()
-     smtp.ehlo()
-     
-     sender = "credenzusser@gmail.com"
-     receiver = "kajalparekh@gmail.com"
-     password = input("Enter your password:- ")
-     
-     smtp.login(sender,password)
-     
-     subject = "Regarding NCC"
-     body = "Thank you participating in NCC 2020. \nHere is your certificate"
-     
-     msg = f'Subject: {subject}\n\n{body}'
-     
-     smtp.sendmail(sender, receiver, msg)
-     
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    sender = "credenzusser@gmail.com"
+    password = getpass.getpass("Enter your password:- ")
 
+    smtp.login(sender, password)
 
+    for i in range(inputWorksheet.nrows):
+        receiver = inputWorksheet.cell_value(i, 3)
+        user1 = inputWorksheet.cell_value(i, 1)
+        user2 = inputWorksheet.cell_value(i, 2)
+        subject = "Regarding NCC"
+        if user2 == '':
+            body = f'Thank you {user1} for participating in NCC 2020'
+        else:
+            body = f'Thank you {user1} and {user2} for participating in NCC 2020.'
+        msg = f'Subject: {subject}\n\n{body}'
+
+        smtp.sendmail(sender, receiver, msg)
+        print(f'Sent mail {i+1}')
