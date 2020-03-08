c36fd46f1b06c414e9eaf65e45dabffe8cbb1388
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-03-08 17:32:22 +0530

    Wrong logic

--- certificate.py
+++ certificate.py
@@ -16,7 +16,10 @@ bcc = []
 for i in range(inputWorksheet.nrows):
     user1.append(inputWorksheet.cell_value(i, 1))
     user2.append(inputWorksheet.cell_value(i, 2))
+    if user2[i] == '':
+        user2[i] == 'null'
     bcc.append(inputWorksheet.cell_value(i, 3))
+
 
 sender = 'credenzusser@gmail.com'
 password = getpass.getpass("Enter your password:- ")
@@ -28,34 +31,22 @@ msg['Bcc'] = ', '.join(bcc)
 msg.set_content(
     "Here is an attachment with the mail.\nPlease do not discose the certificate elsewhere.")
 
-PaintCertificate(user1, user2)
+for j in range(inputWorksheet.nrows):
+    files = [f'{user1[j]}.png', f'{user2[j]}.png']
 
-files = ['certificate1.jpeg', 'certificate2.jpeg']
+    for file in files:
+        if file == "null":
+            pass
+        else:
+            with open(file, 'rb') as image:
+                file_data = image.read()
+                file_type = imghdr.what(image.name)
+                file_name = image.name
 
-for file in files:
-    with open(file, 'rb') as image:
-        file_data = image.read()
-        file_type = imghdr.what(image.name)
-        file_name = image.name
+            msg.add_attachment(file_data, maintype='image',
+                               subtype=file_type, filename=file_name)
 
-    msg.add_attachment(file_data, maintype='image',
-                       subtype=file_type, filename=file_name)
-
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(sender, password)
-    smtp.send_message(msg)
-    print("Sent mails")
-
-
-def PaintCertificate(user1, user2):
-
-    image = Image.open('certificate1.jpeg')
-    font = ImageFont.truetype(
-        '/usr/share/fonts/truetype/freefont/FreeMono.ttf', size=25)
-
-    draw = ImageDraw.Draw(image)
-    draw.text(xy=(525, 430), text=user1[0], fill=(0, 0, 0), font=font)
-    image.save(f'{user1[0]}.png')
-    draw.text(xy=(525, 430), text=user2[0], fill=(0, 0, 0), font=font)
-    image.save(f'{user2[0]}.png')
-    print("Done")
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(sender, password)
+        smtp.send_message(msg)
+    print(f'Sent mail {j+1}')
--- paint.py
+++ paint.py
@@ -13,14 +13,19 @@ for i in range(inputWorksheet.nrows):
     user2.append(inputWorksheet.cell_value(i, 2))
     bcc.append(inputWorksheet.cell_value(i, 3))
 
+for i in range(inputWorksheet.nrows):
+    image1 = Image.open('certificate.jpeg')
+    image2 = Image.open('certificate.jpeg')
 
-image = Image.open('certificate1.jpeg')
-font = ImageFont.truetype(
-    '/usr/share/fonts/truetype/freefont/FreeMono.ttf', size=25)
-
-draw = ImageDraw.Draw(image)
-draw.text(xy=(525, 430), text=user1[0], fill=(0, 0, 0), font=font)
-image.save(f'{user1[0]}.png')
-draw.text(xy=(525, 430), text=user2[0], fill=(0, 0, 0), font=font)
-image.save(f'{user2[0]}.png')
-print("Done")
+    font = ImageFont.truetype(
+        '/usr/share/fonts/truetype/freefont/FreeMono.ttf', size=25)
+    draw = ImageDraw.Draw(image1)
+    draw.text(xy=(525, 430), text=user1[i], fill=(0, 0, 0), font=font)
+    image1.save(f'{user1[i]}.png')
+    draw = ImageDraw.Draw(image2)
+    draw.text(xy=(525, 430), text=user2[i], fill=(0, 0, 0), font=font)
+    if user2[i] == '':
+        pass
+    else:
+        image2.save(f'{user2[i]}.png')
+    print(f'Generated for {user1[i]} and {user2[i]}')
