c923fc7ffa5241d1b99d99a197545c2711bbe719
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-03-08 19:29:55 +0530

    Another file

--- certificate.py
+++ certificate.py
@@ -3,7 +3,6 @@ import smtplib
 import getpass
 import imghdr
 from email.message import EmailMessage
-from PIL import Image, ImageDraw, ImageFont
 
 
 path = "ncc.xlsx"
@@ -31,22 +30,21 @@ msg['Bcc'] = ', '.join(bcc)
 msg.set_content(
     "Here is an attachment with the mail.\nPlease do not discose the certificate elsewhere.")
 
-for j in range(inputWorksheet.nrows):
-    files = [f'{user1[j]}.png', f'{user2[j]}.png']
+files = [f'{user1[0]}.png', f'{user2[0]}.png']
 
-    for file in files:
-        if file == "null":
-            pass
-        else:
-            with open(file, 'rb') as image:
-                file_data = image.read()
-                file_type = imghdr.what(image.name)
-                file_name = image.name
+for file in files:
+    if file == "null":
+        pass
+    else:
+        with open(file, 'rb') as image:
+            file_data = image.read()
+            file_type = imghdr.what(image.name)
+            file_name = image.name
 
-            msg.add_attachment(file_data, maintype='image',
-                               subtype=file_type, filename=file_name)
+        msg.add_attachment(file_data, maintype='image',
+                           subtype=file_type, filename=file_name)
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(sender, password)
-        smtp.send_message(msg)
-    print(f'Sent mail {j+1}')
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(sender, password)
+    smtp.send
+print(f'Sent mails')
---
+++ certificate2.py
@@ -0,0 +1,35 @@
+import xlrd
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+sender = 'credenzusser@gmail.com'
+receiver = 'kaustubhodak1@gmail.com'
+subject = 'This is just a test'
+
+msg = MIMEMultipart()
+msg['From'] = sender
+msg['To'] = receiver
+msg['Subject'] = subject
+
+body = 'Here is an attachment with the mail.\nPlease do not discose the certificate elsewhere.'
+msg.attach(MIMEText(body, 'plain'))
+
+filename = 'certificate.jpeg'
+attachment = open(filename, 'rb')
+
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Conrent-Disposition', 'attachement; filename= '+filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(sender, "credenztechdays")
+
+server.sendmail(sender, receiver, text)
+server.quit()
