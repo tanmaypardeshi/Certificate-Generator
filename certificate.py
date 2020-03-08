3e2fa272f9356ab7b980862b526c2b2fcea92522
tanmaypardeshi <tanmaypardeshi@gmail.com>
2020-03-08 16:33:04 +0530

    2 Attachements

--- certificate.py
+++ certificate.py
@@ -23,12 +23,16 @@ msg['Bcc'] = ', '.join(bcc)
 msg.set_content(
     "Here is an attachment with the mail.\nPlease do not discose the certificate elsewhere.")
 
-with open('certificate.jpeg', 'rb') as image:
-    file_data = image.read()
-    file_type = imghdr.what(image.name)
-    file_name = image.name
+files = ['certificate1.jpeg', 'certificate2.jpeg']
 
-msg.add_attachment(file_data, maintype='image', subtype=file_type)
+for file in files:
+    with open(file, 'rb') as image:
+        file_data = image.read()
+        file_type = imghdr.what(image.name)
+        file_name = image.name
+
+    msg.add_attachment(file_data, maintype='image',
+                       subtype=file_type, filename=file_name)
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(sender, password)
